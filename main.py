bb3586a2c870c76a5969ba00ee827315e2de330a
Nick <nikolaysn@gmail.com>
2024-05-29 21:46:58 +0300

    сделал Router для handlers навел первые порядки
    спрятол ТОКЕН

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+import os
 import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.types import  Message
 
-bot = Bot(token= '5881506855:AAGzrrsifHuhx8HGgQbon10YCdyxjlpef0A')
-dp = Dispatcher()
 
-@dp.message()
-async def cmd_start(message: Message):
-    await message.answer('Привет!')
+from dotenv import load_dotenv
+
+from app.handlers import router
+
+
+
+
 
 async def main():
+    load_dotenv()
+    bot = Bot(token=os.getenv('TOKEN'))
+    dp = Dispatcher()
+    dp.include_router(router)
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('бот выключен')
