fa4f56dc6a0d49e9eb60c1166387d0eb7dadf255
Nick <nikolaysn@gmail.com>
2024-06-01 23:19:18 +0300

    добавил БД

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
 
 from app.handlers import router
+from database.models import async_main
 
 async def main():
     load_dotenv()
+    await async_main()
     bot = Bot(token=os.getenv('TOKEN'))
     dp = Dispatcher()
     dp.include_router(router)
